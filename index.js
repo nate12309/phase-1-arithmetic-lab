f59b7549589cfbd1268f3010d80a3aeec7e01ef8
Nate Horne <nate12309@gmail.com>
2023-07-17 23:41:54 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
-// Write your code here
+// Write your code h
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
+console.log(multiply); // output: 62
+
+// Radom Interger
+const random = Math.floor(Math.random() * 100) + 1; 
+console.log(random) // output random integer greater than 0
+
+// find remainder 
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4;
+console.log(mod); // Output: 4
+
+//find the highest numnber in a set 
+const numbers = [5, 10, 20, 15, 7];
+const max = Math.max(...numbers);
+console.log(max); // Output: 20
